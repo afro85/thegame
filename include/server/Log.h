7afa4904d8d992184b354d39fe61a98763d0d1f7
Piotr Piwko <piotr.piwko@gmail.com>
2016-11-25 16:12:38 +0100

    Server: Initial version of Log module

---
+++ include/server/Log.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+
+class Log
+{
+public:
+    static std::ostream& i() {return std::cout << "I: ";}
+    static std::ostream& d() {return std::cout << "D: ";}
+    static std::ostream& w() {return std::cout << "W: ";}
+    static std::ostream& e() {return std::cout << "E: ";}
+};
